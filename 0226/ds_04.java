96bbb463e728fdfc3a91438f6efa9ea9f571bf76
Yu-Li115 <412637737@o365.tku.edu.tw>
2025-06-26 12:37:04 +0800

    Create ds_04.java

---
+++ 0226/ds_04.java
@@ -0,0 +1,9 @@
+public class ds_04 {
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3, 4, 5};
+
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println("元素" + i + ":" + numbers[i]);
+        }
+    }
+}
