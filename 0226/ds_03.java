640dfb0064faa81150443ea08b70481245b089a9
Yu-Li115 <412637737@o365.tku.edu.tw>
2025-07-03 13:19:03 +0800

    Update ds_03.java

--- 0226/ds_03.java
+++ 0226/ds_03.java
@@ -2,21 +2,22 @@
 import java.util.Scanner;
 
 public class ds_03 {
+
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.println("輸入姓名: ");
+            String name = scanner.nextLine();
 
-        System.out.println("輸入姓名: ");
-        String name = scanner.nextLine();
+            System.out.println("輸入年齡: ");
+            int age = scanner.nextInt();
+            scanner.nextLine();
 
-        System.out.println("輸入年齡: ");
-        int age = scanner.nextInt();
-        scanner.nextLine();
+            System.out.println("輸入城市: ");
+            String city = scanner.nextLine();
 
-        System.out.println("輸入城市: ");
-        String city = scanner.nextLine();
-
-        System.out.println("姓名: " + name);
-        System.out.println("年齡: " + age);
-        System.out.println("城市: " + city);
+            System.out.println("姓名: " + name);
+            System.out.println("年齡: " + age);
+            System.out.println("城市: " + city);
+        }
     }
 }
