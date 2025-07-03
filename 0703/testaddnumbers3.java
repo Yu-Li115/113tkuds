1f362636addcfe4a5481c6c63796a4f4909cdea2
Yu-Li115 <412637737@o365.tku.edu.tw>
2025-07-03 13:19:44 +0800

    Delete testaddnumbers3.java

--- 0703/testaddnumbers3.java
+++
@@ -1,8 +0,0 @@
-public class testaddnumbers3 {
-    public static void main(String[] args) {
-        int num1 = 12;  
-        int num2 = 26;  
-        int result = num1 + num2;  
-        System.out.println(result);  
-    }
-}
