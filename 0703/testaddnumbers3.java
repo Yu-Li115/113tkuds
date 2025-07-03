c272cc128555f39d170d5353dca3f2966f21a1e9
Yu-Li115 <412637737@o365.tku.edu.tw>
2025-07-03 12:28:56 +0800

    Create testaddnumbers3.java

---
+++ 0703/testaddnumbers3.java
@@ -0,0 +1,8 @@
+public class testaddnumbers3 {
+    public static void main(String[] args) {
+        int num1 = 12;  
+        int num2 = 26;  
+        int result = num1 + num2;  
+        System.out.println(result);  
+    }
+}
