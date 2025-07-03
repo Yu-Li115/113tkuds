457c21003d7b54b131eb8f58d06ccfc6a6bfbb65
Yu-Li115 <412637737@o365.tku.edu.tw>
2025-07-03 13:19:23 +0800

    Update printname.java

--- 0703/printname.java
+++ 0703/printname.java
@@ -1,9 +1,12 @@
+
 import java.util.Scanner;
 
 public class printname {
+
     public static void main(String[] args) {
-        Scanner  scanner = new Scanner(System.in);
-        String name = scanner.nextLine();
-        System.out.println(name);
+        try (Scanner scanner = new Scanner(System.in)) {
+            String name = scanner.nextLine();
+            System.out.println(name);
+        }
     }
 }
