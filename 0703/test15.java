9c3da40a9578d0fe923e63b79a94d4f1018f5bf7
Yu-Li115 <412637737@o365.tku.edu.tw>
2025-07-03 22:22:55 +0800

    Create test15.java

---
+++ 0703/test15.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class test15 {
+    public static void main(String[] args) {
+        try (Scanner sc = new Scanner(System.in)) {
+            int N = sc.nextInt(); 
+            int M = sc.nextInt(); 
+
+            int[][] matrix = new int[N][M];
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < M; j++) {
+                    matrix[i][j] = sc.nextInt();
+                }
+            }
+
+            for (int j = 0; j < M; j++) {
+                for (int i = 0; i < N; i++) {
+                    System.out.print(matrix[i][j]);
+                    if (i != N - 1) System.out.print(" ");
+                }
+                System.out.println();
+            }
+        }
+        
+    }
+}
