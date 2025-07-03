4b57fc00f301cb866b2b660f7fac4b2170fb2c5d
Yu-Li115 <412637737@o365.tku.edu.tw>
2025-07-03 13:18:59 +0800

    Update ds_02.java

--- 0226/ds_02.java
+++ 0226/ds_02.java
@@ -1,14 +1,16 @@
+
 import java.util.Scanner;
 
 public class ds_02 {
+
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.print("請輸入半徑: ");
+            double radius = scanner.nextDouble();
 
-        System.out.print("請輸入半徑: ");
-        double radius = scanner.nextDouble();
+            double area = Math.PI * radius * radius;
 
-        double area = Math.PI * radius * radius;
-
-        System.out.println("面積是: " + area);
+            System.out.println("面積是: " + area);
+        }
     }
 }
