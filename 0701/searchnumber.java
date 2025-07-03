383b9e1d11c3d6525e8fdb99f72c6526a7385324
Yu-Li115 <412637737@o365.tku.edu.tw>
2025-07-03 13:19:16 +0800

    Update searchnumber.java

--- 0701/searchnumber.java
+++ 0701/searchnumber.java
@@ -1,30 +1,29 @@
+
 import java.util.Scanner;
 
 public class searchnumber {
+
     public static void main(String[] args) {
-        int[] s = {1, 3, 5, 7, 9, 11, 13}; // 已排序的數列
-        Scanner scanner = new Scanner(System.in);
+        int[] s = {1, 3, 5, 7, 9, 11, 13};
 
-        System.out.print("請輸入要查找的數字：");
-        int x = scanner.nextInt();
-        boolean found = false;
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.print("請輸入要查找的數字：");
+            int x = scanner.nextInt();
+            boolean found = false;
 
-        // 線性搜尋
-        for (int num : s) {
-            System.out.println("比對中:x = " + x + "，目前元素 = " + num);
-            if (num == x) {
-                found = true;
-                break;
+            for (int num : s) {
+                System.out.println("比對中:x = " + x + "，目前元素 = " + num);
+                if (num == x) {
+                    found = true;
+                    break;
+                }
+            }
+
+            if (found) {
+                System.out.println("Yes");
+            } else {
+                System.out.println("No");
             }
         }
-
-        // 輸出結果
-        if (found) {
-            System.out.println("Yes");
-        } else {
-            System.out.println("No");
-        }
-
-        scanner.close();
     }
 }
