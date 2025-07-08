38bcb56b40677655f73c8c997b2644acf1dcfeea
Yu-Li115 <412637737@o365.tku.edu.tw>
2025-07-08 11:52:01 +0800

    Update test25.java

--- 0703/test25.java
+++ 0703/test25.java
@@ -1,6 +1,8 @@
+
 import java.util.Scanner;
 
-public class Main {
+public class test25 {
+
     public static void main(String[] args) {
         try (Scanner sc = new Scanner(System.in)) {
             int n = sc.nextInt();
