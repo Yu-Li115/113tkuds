36a0c254df1535f75120a1b4d8dc48f5ab43cad0
Yu-Li115 <412637737@o365.tku.edu.tw>
2025-07-08 09:55:28 +0800

    Update test22.java

---
+++ 0703/test22.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class test22 {
+    public static void main(String[] args) {
+        try (Scanner sc = new Scanner(System.in)) {
+            int N = sc.nextInt();
+            int sum = 0;
+
+            for (int i = 1; i <= N; i++) {
+                if (i % 2 == 0) {
+                    sum += i;
+                }
+            }
+
+            System.out.println(sum);
+        } catch (Exception e) {
+            System.out.println("輸入錯誤：" + e.getMessage());
+        }
+    }
+}
