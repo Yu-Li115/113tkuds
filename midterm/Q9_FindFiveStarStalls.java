0cbb1d568fcc6d0d1ca63ce7f9daf02c21f421ca
Yu-Li115 <412637737@o365.tku.edu.tw>
2025-07-15 11:38:49 +0800

    Create Q9_FindFiveStarStalls.java

---
+++ midterm/Q9_FindFiveStarStalls.java
@@ -0,0 +1,43 @@
+
+import java.util.Scanner;
+
+public class Q9_FindFiveStarStalls {
+
+    public static void main(String[] args) {
+        try (Scanner sc = new Scanner(System.in)) {
+            int n = sc.nextInt();
+            int[] scores = new int[n];
+            for (int i = 0; i < n; i++) {
+                scores[i] = sc.nextInt();
+            }
+
+            int count = 0;
+            for (int score : scores) {
+                if (score == 5) {
+                    count++;
+                }
+            }
+
+            if (count == 0) {
+                System.out.println("None");
+                return;
+            }
+
+            int[] indexes = new int[count];
+            int idx = 0;
+            for (int i = 0; i < n; i++) {
+                if (scores[i] == 5) {
+                    indexes[idx++] = i;
+                }
+            }
+
+            for (int i = 0; i < count; i++) {
+                System.out.print(indexes[i]);
+                if (i < count - 1) {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
