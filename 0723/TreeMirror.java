5edf129fa7726bdfd5dc955caf68143b28748182
Yu-Li115 <412637737@o365.tku.edu.tw>
2025-07-23 16:24:27 +0800

    Create TreeMirror.java

---
+++ 0723/TreeMirror.java
@@ -0,0 +1,79 @@
+
+public class TreeMirror {
+
+    public static class TreeNode {
+
+        int val;
+        TreeNode left, right;
+
+        public TreeNode(int val) {
+            this.val = val;
+        }
+    }
+
+    public static boolean isSymmetric(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        return isMirror(root.left, root.right);
+    }
+
+    private static boolean isMirror(TreeNode t1, TreeNode t2) {
+        if (t1 == null && t2 == null) {
+            return true;
+        }
+        if (t1 == null || t2 == null) {
+            return false;
+        }
+        return (t1.val == t2.val) && isMirror(t1.left, t2.right) && isMirror(t1.right, t2.left);
+    }
+
+    public static void mirror(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+        mirror(root.left);
+        mirror(root.right);
+    }
+
+    public static boolean areMirrors(TreeNode root1, TreeNode root2) {
+        if (root1 == null && root2 == null) {
+            return true;
+        }
+        if (root1 == null || root2 == null) {
+            return false;
+        }
+        if (root1.val != root2.val) {
+            return false;
+        }
+        return areMirrors(root1.left, root2.right) && areMirrors(root1.right, root2.left);
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(2);
+        root.left.left = new TreeNode(3);
+        root.left.right = new TreeNode(4);
+        root.right.left = new TreeNode(4);
+        root.right.right = new TreeNode(3);
+
+        System.out.println(isSymmetric(root)); // true
+
+        mirror(root);
+        System.out.println(isSymmetric(root)); // false after mirror
+
+        TreeNode root2 = new TreeNode(1);
+        root2.left = new TreeNode(2);
+        root2.right = new TreeNode(2);
+        root2.left.left = new TreeNode(4);
+        root2.left.right = new TreeNode(3);
+        root2.right.left = new TreeNode(3);
+        root2.right.right = new TreeNode(4);
+
+        System.out.println(areMirrors(root, root2)); // true
+    }
+}
